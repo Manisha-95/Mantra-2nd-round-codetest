21e7e38d39936e3cf39d1e0bc1d3866cf24d7a15
Manisha-95 <raosmanisha@gmail.com>
2020-07-11 14:57:05 +0530

    3 files modified

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 // Import stylesheets
 import './style.css';
-
-// Write Javascript code!
-$(function () {
-        $("#ddlFruits").change(function () {
-            var selectedText = $(this).find("option:selected").text();
-            var selectedValue = $(this).val();
-            alert("Selected Text: " + selectedText + " Value: " + selectedValue);
-        });
-    });
+var c= {
+    title:{
+        en:"Title",
+        es:"título"
+    },
+    message:{
+        en:"Message goes here",
+        es:"El mensaje va aquí"
+    }
+}
+var k=Object.values(c);
+//console.log(k);
+$('#cmbMoreFunction').click(function(v){
+  //console.log(v.target.value);
+  if(v.target.value=="english")
+k.map(x=>console.log(x.en));
+else if(v.target.value=="spanish")
+k.map(x=>console.log(x.es));
+  }
+  );
 
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
