70e391653d404cd69be152aa6080721ab6019f9f
Manisha-95 <raosmanisha@gmail.com>
2020-07-11 13:51:08 +0530

    1 file modified

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ import './style.css';
 // Write Javascript code!
 var a=document.getElementsByTagName("option")[0];
 console.log(a);
-var m=document.getElementById("mm").options.length;
+var m=document.getElementById("mm");
 console.log(m);
 document.getElementById("mm").onclick=function()
 {
-for(var i=0;i<m;i++)
-{
-
-}
+  console.log("mmk");
+// for(var i=0;i<m;i++)
+// {
+// if(document.getElementById("mm"))
+// }
 }
 //a.onclick=function(){console.log("mmm")}
 const appDiv = document.getElementById('app');
