33b18437ecb7e96b7ac53b5b2cdba9ce5c4e2152
Manisha-95 <raosmanisha@gmail.com>
2020-07-11 14:08:08 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,18 +2,13 @@
 import './style.css';
 
 // Write Javascript code!
-var a=document.getElementsByTagName("option")[0];
-console.log(a);
-var m=document.getElementById("mm");
-console.log(m);
-document.getElementsByTagName("option").onclick=function()
-{
-  console.log("mmk");
-// for(var i=0;i<m;i++)
-// {
-// if(document.getElementById("mm"))
-// }
-}
-//a.onclick=function(){console.log("mmm")}
+$(function () {
+        $("#ddlFruits").change(function () {
+            var selectedText = $(this).find("option:selected").text();
+            var selectedValue = $(this).val();
+            alert("Selected Text: " + selectedText + " Value: " + selectedValue);
+        });
+    });
+
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
