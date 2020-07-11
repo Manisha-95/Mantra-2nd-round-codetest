a83793a7fcec5b5e938cbac151e1226dbfdeaae2
Manisha-95 <raosmanisha@gmail.com>
2020-07-11 13:45:39 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,5 +2,14 @@
 import './style.css';
 
 // Write Javascript code!
+var a=document.getElementsByTagName("option")[0];
+console.log(a);
+var m=document.getElementById("mm").options.length;
+console.log(m);
+for(var i=0;i<m;i++)
+{
+
+}
+//a.onclick=function(){console.log("mmm")}
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
